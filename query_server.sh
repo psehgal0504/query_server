69b080fa4bb6c90696c47288aad2bb9c889fb39a
psehgal <54785569+psehgal0504@users.noreply.github.com>
2019-09-17 09:04:16 -0700

    added options -1 and --help to query_server.sh

--- query_server.sh
+++ query_server.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/bash
 var=$1
 pass="SUCCESS"
+help() {
+    echo "USAGE: query_server.sh [OPTION]"
+    echo "Prints the name of the current server as: "Current Server:[servername]""
+    echo "      -1 Prints the username with the server name"
+    echo "Exit status:"
+    echo "SUCCESS if OK"
+    echo "FAILURE if Problem with the command line"
+}
 
-if [ -z "$var" ]
+if [[ -z "$var" ]]
 then
     var="Current server: $HOSTNAME"
     echo $pass
     echo $var
+elif [[ $var = -1 ]]
+then
+    var="Current server: $USER@$HOSTNAME"
+    echo $pass
+    echo $var
+elif [[ $var = "--help" ]]
+then
+    help
 else
     pass="FAILURE"
     echo $pass
-    echo "ERROR MESSAGE: Type nothing as argument for function"
+    echo "ERROR MESSAGE: Type nothing or -1 for function, otherwise type --help for more information"
 fi
+
